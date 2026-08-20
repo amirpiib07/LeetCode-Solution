18f6c8af7674443ae0f097a92e2384a756505751
AMIT KUMAR SINGH <amirpiib07@gmail.com>
2026-08-20 22:58:37 +0530

    [2075] [Time Beats: 47.62%] [Memory Beats: 68.25%] - LeetPush

--- 2075-decode-the-slanted-ciphertext/2075-Decode-the-Slanted-Ciphertext.cpp
+++ 2075-decode-the-slanted-ciphertext/2075-Decode-the-Slanted-Ciphertext.cpp
@@ -3,23 +3,14 @@ class Solution {
         int n = encodedText.length();
         int cols = n / rows;
 
-        char[][] mat = new char[rows][cols];
-        int idx = 0;
-
-        // Fill matrix
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                mat[i][j] = encodedText.charAt(idx++);
-            }
-        }
-
         StringBuilder sb = new StringBuilder();
 
-        // Traverse diagonals
         for (int j = 0; j < cols; j++) {
             int i = 0, k = j;
+
             while (i < rows && k < cols) {
-                sb.append(mat[i][k]);
+                int index = i * cols + k;
+                sb.append(encodedText.charAt(index));
                 i++;
                 k++;
             }
