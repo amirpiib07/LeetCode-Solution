8b75450afdd2b6885ee4a68b489de6be3c83e697
AMIT KUMAR SINGH <amirpiib07@gmail.com>
2026-08-21 15:09:56 +0530

    [3116] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

--- DCP-08-26/3116-kth-smallest-amount-with-single-denomination-combination/3116-Kth-Smallest-Amount-With-Single-Denomination-Combination.cpp
+++ DCP-08-26/3116-kth-smallest-amount-with-single-denomination-combination/3116-Kth-Smallest-Amount-With-Single-Denomination-Combination.cpp
@@ -1,39 +1,24 @@
 class Solution {
 private:
-    long long gcd(long long a, long long b) {
-        return b == 0 ? a : gcd(b, a % b);
-    }
-
-    long long lcm(long long a, long long b) {
-        return (a / gcd(a, b)) * b;
-    }
-
-    long long f(long long mid, const vector<long long>& coins) {
+    int f(int mid, vector<int>& coins){
         int n = coins.size();
-        long long ans = 0;
+        int ans = 0;
 
-        for (int mask = 1; mask < (1 << n); mask++) {
-            long long curr_lcm = 1;
+        for(int mask = 1; mask < (1<<n); mask++){
+            int curr_lcm = 1;
             int curr_size = 0;
-            bool overflow = false;
-
-            for (int i = 0; i < n; i++) {
-                if ((mask & (1 << i)) != 0) {
+            for(int i = 0; i < n; i++){
+                if((mask & (1<<i))!= 0){
                     curr_lcm = lcm(curr_lcm, coins[i]);
                     curr_size++;
-                    if (curr_lcm > mid) {
-                        overflow = true;
-                        break;
-                    }
+                    if(curr_lcm > mid) break;
                 }
             }
 
-            if (overflow) continue;
-
-            if (curr_size & 1) {
-                ans += mid / curr_lcm;
-            } else {
+            if((curr_size & 1) == 0){
                 ans -= mid / curr_lcm;
+            } else{
+                ans += mid / curr_lcm;
             }
         }
 
@@ -42,34 +27,19 @@ private:
 
 public:
     long long findKthSmallest(vector<int>& coins, int k) {
-        sort(coins.begin(), coins.end());
-        
-        
-        vector<long long> filtered;
-        for (int i = 0; i < coins.size(); i++) {
-            bool redundant = false;
-            for (int j = 0; j < filtered.size(); j++) {
-                if (coins[i] % filtered[j] == 0) {
-                    redundant = true;
-                    break;
-                }
-            }
-            if (!redundant) {
-                filtered.push_back(coins[i]);
-            }
-        }
+        int n = coins.size();
+        int mn = *min_element(coins.begin(), coins.end());
 
-        long long low = 1;
-        long long high = filtered[0] * (long long)k;
-        long long ans = high;
+        long long low = 1, high = k * (long long)mn;
+        long long ans = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
 
-        while (low <= high) {
-            long long mid = low + (high - low) / 2; 
-
-            if (f(mid, filtered) >= k) {
+            if(f(mid, coins) >= k){
                 ans = mid;
                 high = mid - 1;
-            } else {
+            }
+            else{
                 low = mid + 1;
             }
         }
