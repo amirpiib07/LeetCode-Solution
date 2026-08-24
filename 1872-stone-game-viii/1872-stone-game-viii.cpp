65bd4853a235e46c3a55e04745b989f07f5d68ae
AMIT KUMAR SINGH <amirpiib07@gmail.com>
2026-08-24 21:56:35 +0530

    Time: 111 ms (44.66%), Space: 87 MB (96.05%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -6,15 +6,16 @@ public:
         for (int idx = 1; idx < stones.size(); idx++)
             stones[idx] = stones[idx] + stones[idx - 1];
 
-        vector<int> dp(n);
-        dp[n - 1] = stones[n - 1];
+        int prev, curr;
+        prev = stones[n - 1];
         for (int idx = n - 2; idx >= 1; idx--) {
-            int take = stones[idx] - dp[idx + 1];
-            int n_take = dp[idx + 1];
+            int take = stones[idx] - prev;
+            int n_take = prev;
 
-            dp[idx] = max(take, n_take);
+            curr = max(take, n_take);
+            prev = curr;
         }
 
-        return dp[1];
+        return prev;
     }
 };
