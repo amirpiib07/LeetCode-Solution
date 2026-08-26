bcec1ca7bbf7062a26d07008a278d2d7892d42b5
AMIT KUMAR SINGH <amirpiib07@gmail.com>
2026-08-26 11:00:00 +0530

    Time: 0 ms (100%), Space: 8.5 MB (98.99%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,37 +2,36 @@ class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         int n = s.size();
-        int len = INT_MAX;
-        int i = 0, j = 0;
-        int start = 0, end = 0;
-        string ans = "";
-        int c_one = 0;
-        while(j < n){
-            if(s[j] == '1') c_one++;
-            // if(c_one == k + 1){
-            //     while(c_one != k){
-            //         if(s[i] == '1') c_one--;
-            //         i++;
-            //     }
-            // }
-            while(c_one == k && i< n){
-                start = i;
-                end = j;
-                if(len > (j - i + 1)) {
-                    len = (j - i + 1);
-                    ans = s.substr(i,len);
-                }
-                if(len == (j - i + 1) && ans > s.substr(i,len)) {
-                    ans = s.substr(i,len);
-                }
-                if(s[i] == '1') c_one--;
+        int bestLen = INT_MAX;
+        int bestStart = -1;
+        int i = 0, c_one = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (s[j] == '1') c_one++;
+
+            
+            while (c_one > k) {
+                if (s[i] == '1') c_one--;
                 i++;
             }
 
-            j++;
+            
+            while (c_one == k) {
+                int curLen = j - i + 1;
+                if (curLen < bestLen) {
+                    bestLen = curLen;
+                    bestStart = i;
+                } else if (curLen == bestLen) {
+                    
+                    if (s.compare(i, bestLen, s, bestStart, bestLen) < 0) {
+                        bestStart = i;
+                    }
+                }
+                if (s[i] == '1') c_one--;
+                i++;
+            }
         }
 
-
-        return ans;
+        return bestStart == -1 ? "" : s.substr(bestStart, bestLen);
     }
 };
